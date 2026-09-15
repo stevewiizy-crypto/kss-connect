1e59b59aab6b2f12041c6f58b91aa142f1c2c986
stevewiizy-crypto <stevewiizy@gmail.com>
2026-09-15 23:57:37 +0300

    Add school and API URL constants to config

--- .github/src/src/src/src/config/index.js
+++ .github/src/src/src/src/config/index.js
@@ -1,1 +1,3 @@
-
+export const SCHOOL={name:"Kingston Senior Secondary School"};
+export const ONLINE_API_URL=import.meta.env.VITE_ONLINE_API_URL||"http://localhost:5000";
+export const LOCAL_API_URL=import.meta.env.VITE_LOCAL_API_URL||"http://192.168.1.100:5000";
