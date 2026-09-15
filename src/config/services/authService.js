abfe3c67947d9da7d7cf1939eb8d6772547267c0
stevewiizy-crypto <stevewiizy@gmail.com>
2026-09-16 02:10:15 +0300

    Delete src/config/services directory

--- src/config/services/authService.js
+++
@@ -1,5 +0,0 @@
-import { supabase } from '../config/supabaseClient.js'
-export const signUp = (e,p) => supabase.auth.signUp({email:e,password:p})
-export const signIn = (e,p) => supabase.auth.signInWithPassword({email:e,password:p})
-export const signOut = () => supabase.auth.signOut()
-export const getUser = () => supabase.auth.getUser()
