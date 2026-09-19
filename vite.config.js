48c28c6c5ab16684b66feb359c8d4a904c8d475b
stevewiizy-crypto <stevewiizy@gmail.com>
2026-09-19 09:18:35 +0300

    Add server and preview configuration to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  }
+})
